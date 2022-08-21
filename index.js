dfb00e83884a7565f1535ad9278218acf32e807c
dennismuturi1@gmail.com <dennis@WIN-AVPLU2QPEMC.localdomain>
2022-08-21 14:59:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+
+function superbowlWin(record){
+    let theElement = record.find(element => element.result === "W");
+    if(theElement){
+        return (theElement.year);
+    }else return (undefined);
+}
